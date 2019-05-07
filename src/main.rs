7bada2e8d62446fe2e8ffa789ba4755602769d19
softprops <d.tangren@gmail.com>
2019-05-06 23:39:08 -0400

    use lambda events crate

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,36 @@
-use serde::Deserialize;
+use aws_lambda_events::event::kinesis::KinesisEvent;
 use lambda_runtime::{error::HandlerError, lambda, Context};
 use std::str::from_utf8;
-
-#[derive(Deserialize, Debug, Default)]
-#[serde(rename_all = "PascalCase")]
-struct Event {
-    pub records: Vec<Record>,
-}
-
-#[derive(Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]
-struct Record {
-    #[serde(rename = "eventID")]
-    event_id: String,
-    event_version: String,
-    kinesis: Kinesis,
-    invoke_identity_arn: String,
-    event_name: String,
-    #[serde(rename = "eventSourceARN")]
-    event_source_arn: String,
-    event_source: String,
-    aws_region: String,
-}
-
-#[derive(Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]
-struct Kinesis {
-    approximate_arrival_timestamp: f64,
-    partition_key: String,
-    // base64 encoded
-    data: String,
-    kinesis_schema_version: String,
-    sequence_number: String,
-}
-
-impl Kinesis {
-    /// base64 decode data
-    fn decoded_data(&self) -> Result<String, base64::DecodeError> {
-        base64::decode(&self.data)
-            .map(|bytes| from_utf8(&bytes).unwrap_or_default().into())
-    }
-}
 
 fn main() {
     lambda!(handler)
 }
 
 fn handler(
-    event: Event,
+    event: KinesisEvent,
     _: Context,
 ) -> Result<(), HandlerError> {
     for record in event.records {
-            println!(
-                "{}",
-                record
-                    .kinesis
-                    .decoded_data()
-                    .unwrap_or_else(|_| "failed to decode record.data".into())
-            );
-        }
+        println!(
+            "{}",
+            base64::decode(&record.kinesis.data.0)
+                .map(|bytes| from_utf8(&bytes).unwrap_or_default().to_owned())
+                .unwrap_or_else(|err| format!("failed to decode record.data {}", err))
+        );
+    }
     Ok(())
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_json::json;
 
     #[test]
     fn handler_handles() {
-       let event = Event {
-            records: vec![
-                Record {
-                    kinesis: Kinesis {
-                        ..Kinesis::default()
-                    },
-                    ..Record::default()
-                }
-            ],
-        };
-        // assert_eq!(
-        //     handler(event.clone(), Context::default()).expect("expected Ok(_) value"),
-        //     event
-        // )
+        let event: KinesisEvent =
+            serde_json::from_slice(include_bytes!("../tests/example-event.json"))
+                .expect("invalid kinesis event");
+
+        assert!(handler(event, Context::default()).is_ok())
     }
 }
